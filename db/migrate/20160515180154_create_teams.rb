53bad9c14c1c31829593c0ecf99d54e573ee75f9
Talal Talhouk <email": null>
2016-05-15 18:32:44 -0700

    add migrations

--- db/migrate/20160515180154_create_teams.rb
+++ db/migrate/20160515180154_create_teams.rb
@@ -1,4 +1,11 @@
 class CreateTeams < ActiveRecord::Migration
   def change
+    create_table :teams do |t|
+      t.string  :name
+      t.string  :sport
+      t.integer :manager_id
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20160515180248_create_stats.rb
+++ db/migrate/20160515180248_create_stats.rb
@@ -1,4 +1,10 @@
 class CreateStats < ActiveRecord::Migration
   def change
+    create_table :stats do |t|
+      t.integer  :player_id
+      t.integer  :goals, :assists, :games_played
+
+      t.timestamps
+    end
   end
 end
